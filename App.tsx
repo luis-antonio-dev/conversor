18425b6f08a82ae8de44b539afb1d7913f9944af
Luis Antonio <luis.developer.ac@gmail.com>
2022-04-25 22:03:21 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 
 export default function App() {
+  const [kelvin, setKelvin] = useState(0);
+  const [temperature, setTemperature] = useState(0);
+
+  function celsiusToKelvin(value: number): number {
+    return value + 273.15;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.result}>Kelvin: {kelvin}</Text>
+
+      <TextInput
+        value={temperature.toString()}
+        onChangeText={(newTemperature) =>
+          setTemperature(
+            newTemperature.length > 0 ? Number.parseFloat(newTemperature) : 0
+          )
+        }
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="°C"
+      />
+
+      <Button
+        title="Calcular"
+        onPress={() => setKelvin(celsiusToKelvin(temperature))}
+      />
     </View>
   );
 }
@@ -13,8 +36,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  result: {
+    fontSize: 35,
+    marginTop: 30,
+    paddingLeft: 30,
+  },
+  input: {
+    marginTop: 30,
+    fontSize: 35,
+    marginBottom: 30,
+    paddingLeft: 30,
+  },
+  button: {
+    width: "90%",
   },
 });
